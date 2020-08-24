0edf6f8ded9617d25f89460f70be28d34dbbbf69
Rhythm Singh <rhysun08@gmail.com>
2020-08-25 00:26:47 +0530

    Updates

---
+++ precache-manifest.a23deb9e9b23027f7d4decca54a2f8c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d8cd5f1127ae6d6b495d2d18f73658",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robo-friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2a07c7f59d4cbee4c020",
+    "url": "/robo-friends/static/css/main.aeac9805.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robo-friends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a07c7f59d4cbee4c020",
+    "url": "/robo-friends/static/js/main.c5480235.chunk.js"
+  },
+  {
+    "revision": "b7276070548a11fb737e",
+    "url": "/robo-friends/static/js/runtime-main.17f36485.js"
+  }
+]);
